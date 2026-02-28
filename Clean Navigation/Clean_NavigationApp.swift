afac01188272eef5616d755527f9359d3e817ba7
Keshavkumar96 <reachoutkeshav@gmail.com>
2026-03-01 00:25:57 +0530

    Initial Commit

---
+++ Clean Navigation/Clean_NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clean_NavigationApp.swift
+//  Clean Navigation
+//
+//  Created by Keshavkumar on 01/03/26.
+//
+
+import SwiftUI
+
+@main
+struct Clean_NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
